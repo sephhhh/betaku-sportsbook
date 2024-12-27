8f7c7747854f02c7752b02ca5edb0b79f407267b
sephhhh <stephenwu07curry@gmail.com>
2024-12-27 14:46:41 -0500

    user is able to add their selections to the betslip

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -8,41 +8,51 @@ import { convertDecimalToAmerican, createGame } from "./oddsApi";
 import gameData from "../../../game.json";
 
 function createItem(game, market, outcome) {
-  const homeTeam = game.home_team;
-  const awayTeam = game.away_team;
+  const homeTeam = game.homeTeam;
+  const awayTeam = game.awayTeam;
   const odds = convertDecimalToAmerican(outcome.price)
   const type = market.key;
   const commence_time = formatToEST(game.commenceTime);
 
-  return (
-    <main>
-      {homeTeam}
-    </main>
-  )
+  if (type === 'h2h') {
+    return (
+      <div className="grid grid-cols-[17px_1fr] gap-[15px] pt-[20px] pb-[20px] border-b border-solid border-[#c8c8c8]">
+        <button className="flex items-start pt-[6px] h-[17px] w-auto">
+            <img src="delete.png" alt="Delete" className="h-[17px] w-auto"/>
+        </button>
+        <div className="flex flex-col w-[100%] box-border">
+          <div className="flex justify-between items-center">
+            <div className="text-[18px] font-semibold">{outcome.name}</div>
+            <div className="font-bold">{odds}</div>
+          </div>
+          <div className="text-[14px] text-[#788490]">MONEYLINE</div>
+          <div className="flex justify-between">
+            <div className="text-[14px]">{homeTeam} @ {awayTeam}</div>
+            <div className="text-[14px] text-[#788490]">{commence_time}</div>
+          </div>
+        </div>
+      </div>
+    )
+  } else if (type === 'spreads') {
+      return type;
+  }
+  return type;
 }
 
-function createGameFromJson(betSlipRef, setCount, count) {
+function createGameFromJson(setBetItems, betItems, setCount, count) {
   const [games, setGames] = useState([]);
-  const [items, setItems] = useState([]);
   const betButton = useRef(null);
 
   const handleBetButton = (game, market, outcome) => (e) => {
     e.preventDefault();
-    function createItem(game, market, outcome) {
-      const homeTeam = game.home_team;
-      const awayTeam = game.away_team;
-      const odds = convertDecimalToAmerican(outcome.price);
-      const type = market.key;
-      const commence_time = formatToEST(game.commenceTime);
-  
-      // Create a new item object
-      const newItem = { homeTeam, awayTeam, odds, type, commence_time };
-  
-      // Add the new item to the list
-      setItems(prevItems => [...prevItems, newItem]);
-    }
+    const item = createItem(game, market, outcome);
 
-    setCount(count + 1);
+    const marketDiv = document.createElement('div');
+    const gameDiv = document.createElement('div');
+    marketDiv.textContent = JSON.stringify(market);
+    gameDiv.textContent = JSON.stringify(game.commenceTime);
+    setBetItems([...betItems, <div key={betItems.length}>{createItem(game, market, outcome)}</div>]);
+    setCount(count + 1)
   }
 
   useEffect(() => {
@@ -192,6 +202,7 @@ export default function Home() {
   const betSlipRef = useRef(null);
   const slipCountRef = useRef(null);
   const [count, setCount] = useState(0);
+  const [betItems, setBetItems] = useState([]);
 
   const handleSignoutClick = (e) => {
     e.preventDefault();
@@ -218,17 +229,38 @@ export default function Home() {
           </div>
           <hr></hr>
           <div className="overflow-auto min-h-[500px] max-h-[740px] pl-[20px] pr-[20px] pt-[10px] pb-[10px] hide-scrollbar">
-            {createGameFromJson(betSlipRef, setCount, count)}
+            {createGameFromJson(setBetItems, betItems, setCount, count)}
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col min-w-[400px] w-[450px] h-[912px] shadow-[rgba(0,0,0,0.24)_0px_3px_8px]">
+      <div className="flex flex-col min-w-[425px] w-[475px] h-[912px] shadow-[rgba(0,0,0,0.24)_0px_3px_8px]">
         <div className="flex items-center border-b-2 border-b-solid border-b-[#c8c8c8] p-[15px] bg-[#fff] mb-[15px]">
           <div ref={slipCountRef} className="w-[30px] h-[30px] bg-blue-500 text-white flex items-center justify-center rounded-full">{count}</div>
           <p className="flex align-center font-semibold">Betslip</p>
         </div>
-        <div ref={betSlipRef} className="overflow-auto h-[650px] border-t-2 border-b-2 border-solid border-[#c8c8c8] p-[15px] bg-[#fff]"></div>
+        <div ref={betSlipRef} className="overflow-auto h-[650px] border-t-2 border-b border-solid border-[#c8c8c8] pl-[20px] pr-[20px] bg-[#fff]">
+          {betItems}
+        </div>
+        <div className="flex justify-between bg-[#fff] p-[15px] border-b-2 border-solid border-[#c8c8c8]">
+          <div className="flex flex-col justify-center border border-solid border-[#788490] rounded-[5px] w-[200px] h-[70px] pl-[10px] ">
+            <label htmlFor="wager" className="text-[14px]">WAGER</label>
+            <div className="flex items-center gap-[3px]">
+              <div className="text-[20px]">$</div>
+              <input type="text" id="wager" name="wager" className="text-[20px] w-[150px] h-[20px] focus:outline-none"/>
+            </div>
+          </div>
+    
+          <img src="./fist.png" alt="fist" className="h-[70px] w-auto"/>
+
+          <div className="flex flex-col justify-center border border-solid border-[#788490] rounded-[5px] w-[200px] h-[70px] pl-[10px]">
+            <label htmlFor="toWin" className="text-[14px]">TO WIN</label>
+            <div className="flex items-center">
+              <div>$</div>
+              <input type="text" id="toWin" name="toWin" className="w-[150px] h-[18px] focus:outline-none"/>
+            </div>
+          </div>
+        </div>
       
 
         <button
